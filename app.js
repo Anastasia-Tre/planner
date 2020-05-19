8f1414472b8f70eb9459eddbbe223d78c8991c35
Anastasiya Trembach <nastia.trembach@gmail.com>
2020-05-19 22:34:01 +0300

    eslint

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict';
 
 const express = require('express');
 const path = require('path');
@@ -8,11 +9,11 @@ const port = 5000;
 const clientPath = path.join(__dirname, 'client');
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use('/api/record', recordRouter);
 app.use(express.static(clientPath));
 
 
 app.listen(port, () => {
-    console.log(`Server has been started on port ${port}`);
-})
+  console.log(`Server has been started on port ${port}`);
+});
--- json_data.js
+++ json_data.js
@@ -1,66 +1,68 @@
+'use strict';
+
 const fs = require('fs');
 
 const Record = require('./record');
-//generateEvents(10);
+// generateEvents(10);
 
-function writeToJSON(record) { 
+function writeToJSON(record) {
 
-    const file = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-    
-    file.push(record);
-    
-    fs.writeFileSync('data.json', JSON.stringify(file, null, 2));
+  const file = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+
+  file.push(record);
+
+  fs.writeFileSync('data.json', JSON.stringify(file, null, 2));
 }
 
 function randomInteger(min, max) {
-    let rand = min + Math.random() * (max - min);
-    return Math.floor(rand);
+  const rand = min + Math.random() * (max - min);
+  return Math.floor(rand);
 }
 
 
 function generateEvents(n) {
 
-    const names = [
-        'Birthday', 'MeetUP', 'Shopping', 'Event', 'Meeting', 
-        'Game', 'Dinner', 'Lunch', 'Weekend', 'Task', 'Walking',
-    ];
+  const names = [
+    'Birthday', 'MeetUP', 'Shopping', 'Event', 'Meeting',
+    'Game', 'Dinner', 'Lunch', 'Weekend', 'Task', 'Walking',
+  ];
 
-    const people = [
-        'Mother', 'Father', 'Friend', 'Sister', 'Brother', 
-        'Aunt', 'Family', 'Friends', 'Boyfriend', 'Girlfriend',
-    ]
+  const people = [
+    'Mother', 'Father', 'Friend', 'Sister', 'Brother',
+    'Aunt', 'Family', 'Friends', 'Boyfriend', 'Girlfriend',
+  ];
 
-    for (let i = 0; i < n; i++) {
-        let name = names[randomInteger(0, names.length)] + ' with '
-                    + people[randomInteger(0, people.length)];
-        let date = new Date(2020, 3, randomInteger(1, 31));
-        let record = new Record(name, date);
-        writeToJSON(record);
-    }
+  for (let i = 0; i < n; i++) {
+    const name = names[randomInteger(0, names.length)] + ' with ' +
+          people[randomInteger(0, people.length)];
+    const date = new Date(2020, 3, randomInteger(1, 31));
+    const record = new Record(name, date);
+    writeToJSON(record);
+  }
 }
 
 
 
 class Calendar {
 
-    showAll() {
-        this.file = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-        return this.file;
-    }
+  showAll() {
+    this.file = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+    return this.file;
+  }
 
-    save(record) {
-        this.file = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-        this.file.push(record);
-        fs.writeFileSync('data.json', JSON.stringify(this.file, null, 2));
-        return record;
-    }
+  save(record) {
+    this.file = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+    this.file.push(record);
+    fs.writeFileSync('data.json', JSON.stringify(this.file, null, 2));
+    return record;
+  }
 
-    remove(id) {
-        this.file = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-        const recordIndex = this.file.findIndex(record => record._id === id);
-        this.file.splice(recordIndex, 1);
-        fs.writeFileSync('data.json', JSON.stringify(this.file, null, 2));
-    }
+  remove(id) {
+    this.file = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+    const recordIndex = this.file.findIndex(record => record._id === id);
+    this.file.splice(recordIndex, 1);
+    fs.writeFileSync('data.json', JSON.stringify(this.file, null, 2));
+  }
 
 }
 

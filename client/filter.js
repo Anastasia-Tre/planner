13b5261f1630dc12db1a0b8ca962b5a3dfa07a58
Anastasiya Trembach <52916015+Anastasia-Tre@users.noreply.github.com>
2020-06-15 15:18:12 +0300

    fix

--- client/filter.js
+++ client/filter.js
@@ -21,8 +21,8 @@ const filterByWeek = (records, date) => {
   const dayOfWeek1 = new Date(date.setDate(date.getDate() - date.getDay() - 1));
   const dayOfWeek2 = new Date(date.setDate(dayOfWeek1.getDate() + 7));
   for (const elem of records) {
-    if (new Date(elem._date) >= dayOfWeek1 &&
-    new Date(elem._date) <= dayOfWeek2) {
+    const elemDate = new Date(elem._date);
+    if (elemDate >= dayOfWeek1 && elemDate <= dayOfWeek2) {
       result.push(elem);
     }
   }
@@ -52,14 +52,17 @@ function sortBy() {
   document.getElementById(param).classList.add('active');
 
   const date = instancesDate[0].date || new Date();
-  records = records.sort((a, b) =>
-    (new Date(a._date) >= new Date(b._date) ? 1 : -1));
-
+  records = records.sort((a, b) => {
+    const aDate = new Date(a._date);
+    const bDate = new Date(b._date);
+    return aDate >= bDate ? 1 : -1;
+  };
+                         
   const parametrs = {
     'filter-day': filterByDay,
     'filter-week': filterByWeek,
     'filter-month': filterByMonth,
-    'filter-all': filterByAll
+    'filter-all': filterByAll,
   };
   const result = parametrs[param](records, date);
   renderRecords(result);
